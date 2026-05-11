d0c98f010a35a77cd0ad1c50cd5a7eb093620212
Eve-Zed <evezed@gmx.de>
2026-05-11 15:52:03 +0200

    SessionManager cleanup

--- scripts/sessions_layer/session_manager.gd
+++ scripts/sessions_layer/session_manager.gd
@@ -1,25 +1,24 @@
 extends Node
 
 const SESSIONS_PATH := "user://sessions"
-
 const INFO_NAME := "session_info.json"
 
 const CREATE_SESSION_ERROR := "Could not create session: "
 
-var _sessions: Dictionary[String, String] = {} #Session_id -> Session_path
-var _active_session: Session = null #So the session doesn't need to be loaded with every operation
+var _sessions: Dictionary[String, String] = {} 	#Session_id -> Session_path
+var _active_session: Session = null 			#So the session doesn't need to be loaded with every operation
 
 var sessions: Dictionary[String, String]: 
 	get:
 		return _sessions.duplicate(true)
-#var num_sessions: int:
-	#get:
-		#return _sessions.size()
 var active_session_id: String:
 	get:
 		if _active_session == null:
 			return ""
 		return _active_session.session_id
+var active_session: Session:
+	get:
+		return _active_session
 
 func _ready() -> void:
 	_sessions = _get_sessions_from_disc()
@@ -32,6 +31,8 @@ func create_session(session_name: String) -> Error:
 	var session := res.value as Session
 	_sessions[session.session_id] = session.session_path
 	_active_session = session
+	Changelog.log("sessions.log","Created new session %s with ID %s at %s" % 
+	[session.session_name, session.session_id, session.session_path])
 	return OK
 
 func load_session(id: String) -> Result:
@@ -43,6 +44,7 @@ func load_session(id: String) -> Result:
 
 func delete_session(id: String) -> Error:
 	FileUtils.remove_recursive(sessions[id])
+	Changelog.log("sessions.log","Deleted session with ID %s at %s" % [id, sessions[id]])
 	_sessions.erase(id)
 	if active_session_id == id:
 		_active_session = null
@@ -57,47 +59,3 @@ func _get_sessions_from_disc() ->  Dictionary[String, String]:
 			if result.error == OK and result.value.has("id"):
 				session_dict[result.value["id"]] = SESSIONS_PATH.path_join(session)
 	return session_dict
-
-#func load_or_create_session(id: String, session_name: String = "", description: String = "", tags: Array[String] = []) -> Result:
-	#if sessions.has(id):
-		#var l_res := Session.load_session(sessions[id])
-		#if l_res.error != OK:
-			#return Result.new(l_res.error)
-		#var session = l_res.value as Session
-		#var err := session.update_session_info(session_name, description, tags)
-		#if err != OK:
-			#return Result.new(err)
-		#return Result.new(OK,session)
-	#var c_res := Session.create_session(session_name, id)
-	#if c_res.error != OK:
-		#return Result.new(c_res.error)
-	#var session_ = c_res.value as Session
-	#var err_ := session_.update_session_info(session_name, description, tags)
-	#if err_ != OK:
-		#return Result.new(err_)
-	#return Result.new(OK,session_)
-
-#func save_file_in_session(file_path: String, session_id: String) -> Error:
-	#if active_session_id != session_id:
-		#var res := load_session(session_id)
-		#if res.value == null:
-			#return res.error
-		#_active_session = res.value
-	#
-	#var lf_res := FileUtils.load_file(file_path)
-	#if lf_res.value == null:
-		#return lf_res.error
-	#var content := lf_res.value as PackedByteArray	
-	#
-	#var err := _active_session.cache.store_file(
-		#file_path.get_file(), content, FileUtils.get_file_priority(file_path))
-	#if err != OK:
-		#return err
-		#
-	#var sr_err := _active_session.cache.save_registry()
-	#if sr_err != OK:
-		##cleanup if index.json can't be updated
-		#_active_session.cache.delete_file(
-			#file_path.get_file(), FileUtils.hash_file(file_path))
-		#return sr_err
-	#return  OK
