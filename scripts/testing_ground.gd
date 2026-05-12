8ab72fdac6addba6f58e6380ff7e6db74346dc08
Eve-Zed <evezed@gmx.de>
2026-05-12 17:40:15 +0200

    Setup TestingGround for FileManager testing

--- scripts/testing_ground.gd
+++ scripts/testing_ground.gd
@@ -1,11 +1,22 @@
 extends Node
 
-
+@onready
+var campaigns_options_button: OptionButton = $OptionButton
+@onready
+var file_dialog: FileDialog = $FileDialog
+@onready
+var button: Button = $Button
 
 func _ready() -> void:
-	
-	#SessionManager.delete_session(SessionManager.sessions.keys()[0])
-	SessionManager.create_session("Honey Heist Oneshot")
-	var res := SessionManager.load_session(SessionManager.sessions.keys()[0])
-	if res.error == OK:
-		print(res.value.session_name)
+	file_dialog.files_selected.connect(_on_file_dialog_confirmed)
+	var id := 0
+	for s in CampaignManager.campaigns:
+		campaigns_options_button.add_item(CampaignManager.campaigns[s].get_file(),id)
+		id += 1
+
+func _on_button_pressed() -> void:
+	file_dialog.visible = true
+
+func _on_file_dialog_confirmed(paths: PackedStringArray) -> void:
+	for p in paths:
+		print(p)
